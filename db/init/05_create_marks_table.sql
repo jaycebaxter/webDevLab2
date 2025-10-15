5095fd966ff0dbf0182dcba336a26ab39c123e0f
jayce.baxter <jayce.baxter@proton.me>
2025-10-14 22:50:54 -0400

    5 and 6 done

--- db/init/05_create_marks_table.sql
+++ db/init/05_create_marks_table.sql
@@ -5,11 +5,11 @@ October 14th 2025
 INFT 2100
 */
 
-DROP TABLE if exists marks,
+DROP TABLE if exists marks;
 
 CREATE TABLE marks (
-    student_id SERIAL references student_id,
-    course_code varchar (9) references course_code,
-    final_mark int (3),
-    achieved_at timestamp current_timestamp
-)
+    student_id bigint references students(student_id) on delete cascade,
+    course_code varchar (9) references courses(course_code) on delete cascade,
+    final_mark int check (final_mark >= 0 and final_mark <=100),
+    achieved_at timestamp default current_timestamp
+);
